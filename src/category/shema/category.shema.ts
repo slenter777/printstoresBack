0e5d6af8edfada7e1a052d4db21a6caa853385a5
Evgeny Frolov <frqqq@gmail.com>
2020-12-31 10:07:46 +0300

    changed schema in category

--- src/category/shema/category.shema.ts
+++ src/category/shema/category.shema.ts
@@ -4,4 +4,5 @@ export const CategorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   key: { type: String, required: true },
+  prefix: { type: String, required: false },
 });
